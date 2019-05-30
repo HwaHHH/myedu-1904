d7f0e2348021c756bb275cf4d8cff3929ee53ab5
黄小川 <james1640@sina.com>
2019-05-30 10:43:22 +0800

    first commit

---
+++ day01/base_type.py
@@ -0,0 +1,36 @@
+
+
+def  def1():
+    print('def1')
+
+def def2():
+    print('def2')
+
+def def3():
+    a = 6
+    b = 'hwa'
+    c = '神仙'
+    # 字符串拼接
+    # print('%s%s%s'%(a,b,c))
+    # print(str(a)+b+c)
+    # 转换数据类型
+    # print(type(str(a)))
+
+def add(num1,num2):
+    print(num1)
+    print(num2)
+    print(num1+num2)
+    return num1+num2
+
+
+if __name__ == '__main__':
+
+    a = add(98,87)
+    print(a)
+
+
+
+
+
+
+
---
+++ day01/home_work.py
@@ -0,0 +1,10 @@
+
+
+
+from day01 import base_type
+
+if __name__ == '__main__':
+    h = base_type.add(70,58)
+    print(h)
+    a = '我等会富瀚微发到合肥空间和'
+    print(a[::-1])
